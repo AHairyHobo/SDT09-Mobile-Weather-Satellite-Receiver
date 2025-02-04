92b7f062141d4e084718ed3033cb57c758d8ffb3
Sam <Daman1599@gmail.com>
2025-02-04 17:13:49 -0500

    Added sidebar and dropdown menu options to live and historical pages, javascript functions set up for additional actions to be imnplemented later

--- SDP Code/buttonListeners.js
+++ SDP Code/buttonListeners.js
@@ -1,19 +1,366 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function sectorDropdownClick() {
+  document.getElementById("sectorDropdown").classList.toggle("show");
+}
+function imageTypeDropdownClick() {
+  document.getElementById("imageTypeDropdown").classList.toggle("show");
+}
+function numImagesDropdownClick() {
+  document.getElementById("numImagesDropdown").classList.toggle("show");
+}
+function timeStepDropdownClick() {
+  document.getElementById("timeStepDropdown").classList.toggle("show");
+}
+
+/*Options for sector selection*/
+function sector1() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector1Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector2() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector2Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector3() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector3Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector4() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector4Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector5() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector5Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector6() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector6Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector7() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector7Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector8() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector8Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector9() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector9Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector10() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector10Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector11() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector11Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector12() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector12Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector13() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector13Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector14() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector14Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector15() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector15Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector16() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector16Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector17() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector17Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector18() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector18Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector19() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector19Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector20() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector20Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector21() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector21Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector22() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector22Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector23() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector23Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function sector24() {
+  var dropButt = document.getElementById("sectorButton");
+  var text = document.getElementById( id="sector24Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+
+/*Options for image type selection*/
+function type1() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type1Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type2() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type2Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type3() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type3Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type4() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type4Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type5() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type5Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type6() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type6Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type7() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type7Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type8() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type8Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type9() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type9Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type10() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type10Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type11() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type11Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type12() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type12Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type13() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type13Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type14() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type14Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type15() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type15Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type16() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type16Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type17() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type17Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type18() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type18Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type19() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type19Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type20() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type20Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type21() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type21Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type22() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type22Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type23() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type23Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function type24() {
+  var dropButt = document.getElementById("imageTypeButton");
+  var text = document.getElementById( id="type24Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+
+/*Options for number of images selection*/
+function num1() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num1Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num2() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num2Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num3() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num3Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num4() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num4Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num5() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num5Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num6() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num6Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num7() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num7Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num8() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num8Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num9() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num9Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num10() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num10Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num11() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num11Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function num12() {
+  var dropButt = document.getElementById("numImagesButton");
+  var text = document.getElementById( id="num12Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+
+/*Options for selecting the time step*/
+function step1() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step1Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function step2() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step2Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function step3() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step3Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function step4() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step4Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function step5() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step5Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+function step6() {
+  var dropButt = document.getElementById("timeStepButton");
+  var text = document.getElementById( id="step6Button").innerText
+  if(dropButt.innerText != text) dropButt.innerText = text;
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
